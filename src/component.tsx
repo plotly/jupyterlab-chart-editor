3a1003ee2305f372c9174130d8f16be5bca5c658
Grant Nestor <grantnestor@gmail.com>
2018-03-09 14:35:57 -0800

    Make codebase prettier

--- src/component.tsx
+++ src/component.tsx
@@ -8,88 +8,87 @@ import 'react-chart-editor/lib/react-chart-editor.css';
 
 let Plot: any;
 
-
 /**
  * The properties for the JSON tree component.
  */
 export interface IProps {
-	data: Object;
-	plotly: any;
-	width?: number;
-	height?: number;
+  data: Object;
+  plotly: any;
+  width?: number;
+  height?: number;
 }
 
 /**
  * The state of the JSON tree component.
  */
 export interface IState {
-	graphDiv: {
-		data?: Object[];
-		layout?: Object;
-	};
-	editorRevision: number;
-	plotRevision: number;
+  graphDiv: {
+    data?: Object[];
+    layout?: Object;
+  };
+  editorRevision: number;
+  plotRevision: number;
 }
 
 /**
  * A component that renders JSON data as a collapsible tree.
  */
 export default class Editor extends React.Component<IProps, IState> {
-	constructor(props: IProps) {
-		super(props);
-		Plot = createPlotComponent(props.plotly);
-		this.state = {
-			graphDiv: {},
-			editorRevision: 0,
-			plotRevision: 0
-		};
-	}
+  constructor(props: IProps) {
+    super(props);
+    Plot = createPlotComponent(props.plotly);
+    this.state = {
+      graphDiv: {},
+      editorRevision: 0,
+      plotRevision: 0
+    };
+  }
 
-	handlePlotUpdate = (graphDiv: any) => {
-		this.setState(({ editorRevision: x }: { editorRevision: number }) => ({
-			editorRevision: x + 1,
-			graphDiv
-		}));
-	};
+  handlePlotUpdate = (graphDiv: any) => {
+    this.setState(({ editorRevision: x }: { editorRevision: number }) => ({
+      editorRevision: x + 1,
+      graphDiv
+    }));
+  };
 
-	handleEditorUpdate = () => {
-		this.setState(({ plotRevision: x }: { plotRevision: number }) => ({
-			plotRevision: x + 1
-		}));
-	};
+  handleEditorUpdate = () => {
+    this.setState(({ plotRevision: x }: { plotRevision: number }) => ({
+      plotRevision: x + 1
+    }));
+  };
 
-	render() {
-		const { data, plotly } = this.props;
-		const dataSourceOptions = Object.keys(data).map(name => ({
-			value: name,
-			label: name,
-		}));
-		return (
-			<div className="container">
-				<PlotlyEditor
-					config={{editable: true}}
-					graphDiv={this.state.graphDiv}
-					onUpdate={this.handleEditorUpdate}
-					revision={this.state.editorRevision}
-					dataSources={data}
-					dataSourceOptions={dataSourceOptions}
-					plotly={plotly}
-					style={{ width: '100%', height: '100%' }}
-				/>
-				<div className="plot">
-					<Plot
-						debug
-						useResizeHandler
-						config={{editable: true}}
-						data={this.state.graphDiv.data}
-						layout={this.state.graphDiv.layout}
-						onUpdate={this.handlePlotUpdate}
-						onInitialized={this.handlePlotUpdate}
-						revision={this.state.plotRevision}
-						style={{ width: '100%', height: '100%' }}
-					/>
-				</div>
-			</div>
-		);
-	}
+  render() {
+    const { data, plotly } = this.props;
+    const dataSourceOptions = Object.keys(data).map(name => ({
+      value: name,
+      label: name
+    }));
+    return (
+      <div className="container">
+        <PlotlyEditor
+          config={{ editable: true }}
+          graphDiv={this.state.graphDiv}
+          onUpdate={this.handleEditorUpdate}
+          revision={this.state.editorRevision}
+          dataSources={data}
+          dataSourceOptions={dataSourceOptions}
+          plotly={plotly}
+          style={{ width: '100%', height: '100%' }}
+        />
+        <div className="plot">
+          <Plot
+            debug
+            useResizeHandler
+            config={{ editable: true }}
+            data={this.state.graphDiv.data}
+            layout={this.state.graphDiv.layout}
+            onUpdate={this.handlePlotUpdate}
+            onInitialized={this.handlePlotUpdate}
+            revision={this.state.plotRevision}
+            style={{ width: '100%', height: '100%' }}
+          />
+        </div>
+      </div>
+    );
+  }
 }
--- src/index.tsx
+++ src/index.tsx
@@ -19,7 +19,6 @@ import Editor from './component';
 
 import '../style/index.css';
 
-
 /**
  * The CSS class to add to the Plotly Widget.
  */
@@ -36,122 +35,128 @@ const CSS_ICON_CLASS = 'jp-MaterialIcon jp-PlotlyIcon';
  */
 export const MIME_TYPE = 'application/vnd.plotly-editor.v1+json';
 
+export class RenderedPlotlyEditor extends Widget
+  implements IRenderMime.IRenderer {
+  /**
+   * Create a new widget for rendering Plotly.
+   */
+  constructor(options: IRenderMime.IRendererOptions) {
+    super();
+    this.addClass(CSS_CLASS);
+    this._mimeType = options.mimeType;
+    if (!Private.loadingPlotly) Private.loadPlotly();
+  }
 
-export class RenderedPlotlyEditor extends Widget implements IRenderMime.IRenderer {
-	/**
-	 * Create a new widget for rendering Plotly.
-	 */
-	constructor(options: IRenderMime.IRendererOptions) {
-		super();
-		this.addClass(CSS_CLASS);
-		this._mimeType = options.mimeType;
-		if (!Private.loadingPlotly) Private.loadPlotly();
-	}
+  /**
+   * Render Plotly into this widget's node.
+   */
+  renderModel(model: IRenderMime.IMimeModel): Promise<void> {
+    return new Promise<void>((resolve, reject) => {
+      const data = model.data[this._mimeType] as string;
+      if (!data) {
+        reject(undefined);
+      }
+      let json: JSONValue;
+      if (typeof data === 'string') {
+        // If data is from document, parse JSON
+        try {
+          json = JSON.parse(data);
+        } catch (error) {
+          const lines = data.split('\n');
+          const delimiter = lines[0].match(/.+(\t|,)/)[1];
+          const rows = dl.read(data, {
+            type: 'dsv',
+            delimiter,
+            parse: 'auto'
+          }) as JSONObject[];
+          json = rows.reduce((result: { [key: string]: JSONArray }, row) => {
+            Object.entries(row).forEach(([key, value]) => {
+              result[key] = (result[key] || []).concat(value);
+            });
+            return result;
+          }, {});
+        }
+      } else {
+        // If data is from notebook output
+        json = data;
+      }
+      ReactDOM.render(
+        <Editor
+          data={json}
+          plotly={Plotly}
+          width={this.node.offsetWidth}
+          height={this.node.offsetHeight}
+        />,
+        this.node,
+        () => {
+          resolve(undefined);
+        }
+      );
+    });
+  }
 
-	/**
-	 * Render Plotly into this widget's node.
-	 */
-	renderModel(model: IRenderMime.IMimeModel): Promise<void> {
-		return new Promise<void>((resolve, reject) => {
-			const data = model.data[this._mimeType] as string;
-			if (!data) {
-				reject(undefined);
-			}
-			let json: JSONValue;
-			if (typeof data === 'string') {
-				// If data is from document, parse JSON
-				try {
-					json = JSON.parse(data);
-				} catch(error) {
-					const lines = data.split('\n');
-					const delimiter = lines[0].match(/.+(\t|,)/)[1];
-					const rows = dl.read(data, { type: 'dsv', delimiter, parse: 'auto' }) as JSONObject[];
-					json = rows.reduce((result: { [key: string]: JSONArray; }, row) => {
-						Object.entries(row).forEach(([key, value]) => {
-							result[key] = (result[key] || []).concat(value);
-						});
-						return result;
-					}, {});
-				}
-			} else {
-				// If data is from notebook output
-				json = data;
-			}
-			ReactDOM.render(<Editor data={json} plotly={Plotly} width={this.node.offsetWidth} height={this.node.offsetHeight} />, this.node, () => {
-				resolve(undefined);
-			});
-		});
-	}
-
-	private _mimeType: string;
+  private _mimeType: string;
 }
-
 
 /**
  * A mime renderer factory for Plotly data.
  */
 export const rendererFactory: IRenderMime.IRendererFactory = {
-	safe: true,
-	mimeTypes: [MIME_TYPE],
-	createRenderer: options => new RenderedPlotlyEditor(options)
+  safe: true,
+  mimeTypes: [MIME_TYPE],
+  createRenderer: options => new RenderedPlotlyEditor(options)
 };
 
-
 const extensions: IRenderMime.IExtension | IRenderMime.IExtension[] = [
-	{
-		id: 'jupyterlab-chart-editor:factory',
-		rendererFactory,
-		rank: 0,
-		dataType: 'string',
-		fileTypes: [
-			{
-				name: 'plotlyEditor',
-				mimeTypes: [MIME_TYPE],
-				extensions: ['.plotly', '.plotly.json'],
-				iconClass: CSS_ICON_CLASS
-			},
-			{
-				name: 'tsv',
-				mimeTypes: ['text/csv'],
-				extensions: ['.tsv']
-			},
-			{
-				name: 'txt',
-				mimeTypes: ['text/plain'],
-				extensions: ['.txt']
-			}
-		],
-		documentWidgetFactoryOptions: {
-			name: 'Plotly Editor',
-			primaryFileType: 'plotlyEditor',
-			fileTypes: ['json', 'plotlyEditor', 'csv', 'tsv', 'txt']
-		}
-	}
+  {
+    id: 'jupyterlab-chart-editor:factory',
+    rendererFactory,
+    rank: 0,
+    dataType: 'string',
+    fileTypes: [
+      {
+        name: 'plotlyEditor',
+        mimeTypes: [MIME_TYPE],
+        extensions: ['.plotly', '.plotly.json'],
+        iconClass: CSS_ICON_CLASS
+      },
+      {
+        name: 'tsv',
+        mimeTypes: ['text/csv'],
+        extensions: ['.tsv']
+      },
+      {
+        name: 'txt',
+        mimeTypes: ['text/plain'],
+        extensions: ['.txt']
+      }
+    ],
+    documentWidgetFactoryOptions: {
+      name: 'Plotly Editor',
+      primaryFileType: 'plotlyEditor',
+      fileTypes: ['json', 'plotlyEditor', 'csv', 'tsv', 'txt']
+    }
+  }
 ];
 
-
 namespace Private {
-  
   declare function require(moduleName: string): string;
-  
+
   /**
    * Is plotly.js being loaded?.
    */
-  export
-  let loadingPlotly = false;
-  
+  export let loadingPlotly = false;
+
   /**
    * Load plotly.js browser script.
    */
-  export
-  function loadPlotly(): void {
+  export function loadPlotly(): void {
     loadingPlotly = true;
     const script = document.createElement('script');
     script.type = 'text/javascript';
     script.text = require('raw-loader!plotly.js/dist/plotly.min.js');
     document.head.appendChild(script);
   }
-  
 }
 
 export default extensions;
